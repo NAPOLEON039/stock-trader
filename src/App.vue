3ab56ee97564106c9f741f081701238e3978e8d4
NAPOLEON039 <nihar.raote@gmail.com>
2018-12-21 18:26:36 +0530

    Added header and added layout to navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="container">
-    <router-view></router-view>
+    <header></header>
+    <div class="row">
+      <div class="col-md-12">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
 export default {
-  
+  components: {
+    header: Header
+  }
 }
 </script>
 
