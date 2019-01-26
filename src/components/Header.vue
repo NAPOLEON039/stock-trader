267b5ccf96691900c6423686ae4ed8b1d2319df1
NAPOLEON039 <nihar.raote@gmail.com>
2019-01-26 18:48:06 +0530

    Added route animation, save and load features

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,13 +12,13 @@
                 <strong class="navbar-text navbar-right">Funds: {{  funds | currency }}</strong>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#" @click="endDay">End Day</a></li>
-                    <li class="dropdown">
+                    <li class="dropdown" :class="{open: isDropdownOpen}" @click="isDropdownOpen = !isDropdownOpen">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                             Save and Load<span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Save Data</a></li>
-                            <li><a href="#">Load Data</a></li>
+                            <li><a href="#" @click="saveData">Save Data</a></li>
+                            <li><a href="#" @click="loadData">Load Data</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -30,6 +30,11 @@
 <script>
 import {mapActions} from 'vuex';
 export default {
+    data() {
+        return {
+            isDropdownOpen: false
+        }
+    },
     computed: {
         funds() {
             return this.$store.getters.funds;
@@ -41,6 +46,18 @@ export default {
         ]),
         endDay() {
             this.randomizeStocks();
+        },
+        saveData() {
+            const data = {
+                funds: this.$store.getters.funds,
+                stockPortoflio: this.$store.getters.stockPortoflio,
+                stocks: this.$store.getters.stocks
+            };
+            //using the put method with Firebase db replaces existing data with a new one. This is fine in this case where we only care about our recent data
+            this.$http.put('data.json', data); 
+        },
+        loadData() {
+            
         }
     }
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+import App from './App.vue'
 import { routes } from './routes';
 import store from './store/store.js';
 
 Vue.config.productionTip = false
+
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://stock-trader-52ddd.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
-})
+});
 
 const router = new VueRouter({
   mode: "history",
